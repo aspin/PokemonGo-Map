8b65c92f8f17687cbe3b3d3129dc959a616f4496
aspin <tsunami70875@gmail.com>
2016-07-19 01:25:22 -0500

    updated start scripts

--- pe.sh
+++ pe.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python example.py -a ptc -u aspin1 -p "a6j&gt{6OeR6Wd7"  -l 'The Park Evanston, Evanston IL' -st 5 -ar 10 -dg
+python example.py -a ptc -u aspin1 -p "a6j&gt{6OeR6Wd7"  -l 'The Park Evanston, Evanston IL' -st 5 -ar 10 -dg -i "Rattata,Pidgey,Weedle"
